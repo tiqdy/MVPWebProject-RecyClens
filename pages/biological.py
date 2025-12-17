2c905d1aa7516662304ee65d86176c17814e5337
tiqdy <atiqahzahrapramudya@gmail.com>
2025-12-17 11:15:11 +0700

    Update biological.py

--- pages/biological.py
+++ pages/biological.py
@@ -7,7 +7,7 @@ st.set_page_config(
     page_title="Biological Waste - RecycLens",
     page_icon="🍂",
     layout="wide",
-    initial_sidebar_state="extended"
+    initial_sidebar_state="collapsed"
 )
 
 # 2. HELPER FUNCTION (IMAGE TO BASE64)
@@ -386,4 +386,5 @@ with col_right:
     </div>
 </div>
     """, unsafe_allow_html=True)
+
 
